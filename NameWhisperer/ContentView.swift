dae53f6c2a3b53086084dcf5e232f5e14a42f23d
derya <derya@boonandmoil.dev>
2023-01-19 10:43:51 +0000

    implements picker view

--- NameWhisperer/ContentView.swift
+++ NameWhisperer/ContentView.swift
@@ -6,27 +6,53 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State var selectedItem: PhotosPickerItem?
+    @State var data: Data?
+    
     var body: some View {
         NavigationView {
             List {
-                // items here
+                if let data = data, let uiImage = UIImage(data: data) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                }
             }
             .navigationTitle("Photos")
             .toolbar {
-                Button {
-                    // import
-                } label: {
-                    Image(systemName: "plus.circle.fill")
-                        .font(.title)
+                PhotosPicker(selection: $selectedItem, matching: .images) {
+                    Text("Pick photo")
                 }
+            }
+            
+        }
+        .onChange(of: selectedItem) { newValue in
+            guard let item = selectedItem else {
+                return
+            }
+            
+            item.loadTransferable(type: Data.self) {
+                result in
+                switch result {
+                case .success(let data):
+                    if let data = data {
+                        self.data = data
+                    } else {
+                        print("data is nil")
+                    }
+                    
+                case .failure(let failure):
+                    fatalError("\(failure)")
                 
+                }
             }
         }
-        
-        
     }
+        
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
